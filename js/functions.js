ea083a1f0a1f0725891ec11f1510a3fcc93294c8
Irina Lagutina <ivl77@yandex.ru>
2024-02-28 23:00:30 +0300

    new function - checking meeting time

--- js/functions.js
+++ js/functions.js
@@ -41,3 +41,22 @@ findPositiveInt('а я томат'); // NaN
 findPositiveInt(2023); // 2023
 findPositiveInt(-1); // 1
 findPositiveInt(1.5); // 15
+
+const convertToDayMinutes = (string) => {
+  const temp = string.split(':');
+  return Number(temp[0] * 60) + Number(temp[1]);
+};
+
+const checkTime = (time1, time2, time3, time4) => {
+  const workStart = convertToDayMinutes(time1);
+  const workEnd = convertToDayMinutes(time2);
+  const meetingStart = convertToDayMinutes(time3);
+  const meetingEnd = meetingStart + time4;
+  return (meetingStart >= workStart && meetingEnd <= workEnd);
+};
+
+checkTime('08:00', '17:30', '14:00', 90); // true
+checkTime('8:0', '10:0', '8:0', 120); // true
+checkTime('08:00', '14:30', '14:00', 90); // false
+checkTime('14:00', '17:30', '08:0', 90); // false
+checkTime('8:00', '17:30', '08:00', 900); // false
