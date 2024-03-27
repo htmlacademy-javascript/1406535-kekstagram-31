8a5459aaa7509fceee62f759a63dcf5994166722
Irina Lagutina <ivl77@yandex.ru>
2024-03-27 20:08:45 +0300

    create upload new image function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 import {initGallery, setSortClick, applySort} from './gallery.js';
 import {debounce} from './util.js';
 import {getData} from './data.js';
+import {loadPhoto} from './load-photo.js';
 import {openUploadModal} from './upload-modal.js';
 
 const RERENDER_DELAY = 500;
 
-document.querySelector('.img-upload__input').addEventListener('change', () => {
+document.querySelector('.img-upload__input').addEventListener('change', (evt) => {
+  loadPhoto(evt);
   openUploadModal();
 });
 
